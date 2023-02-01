adbf54425ab812511e38a5eb074d0672fda6bb5e
Harper Foley <harper.e.foley@gmail.com>
2023-01-31 18:35:59 -0800

    first commit

---
+++ books/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Book(models.Model):
+    book_owner = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='books',
+        default=None,
+        null=True,
+        blank=True
+    )
+    isbn = models.CharField(
+        'ISBN',
+        max_length=13,
+        unique=True,
+        help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>'
+    )
+    title = models.CharField(max_length=300)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=64)
+    summary = models.TextField(help_text='Enter a brief description of the book.')
+    notes = models.TextField()
+    date_read = models.DateTimeField(auto_now_add=True)
+    entry_updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
